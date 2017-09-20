3ba891df7f092bddf66006659b386ae965a629de
yubing <29988076@qq.com>
2017-09-20 14:28:18 +0800

    ..

---
+++ js/carAbout.js
@@ -0,0 +1,18 @@
+$(function() {
+	//百度地图	
+   var map = new BMap.Map("allmap");
+	var point = new BMap.Point(116.331398,39.897445);
+	map.centerAndZoom(point,12);
+	// 创建地址解析器实例
+	var myGeo = new BMap.Geocoder();
+	// 将地址解析结果显示在地图上,并调整地图视野
+	myGeo.getPoint("北京市海淀区东北旺西路软件园二期华胜天成科研大楼", function(point){
+		if (point) {
+			map.centerAndZoom(point, 16);
+			map.addOverlay(new BMap.Marker(point));
+		}else{
+			alert("您选择地址没有解析到结果!");
+		}
+	}, "北京市");
+
+})
--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(function() {
 	//轮播
 	$(".slideBox").slide({mainCell:".bd ul",effect:"fold",autoPlay:true,trigger:"click",interTime:"6000",delayTime:1000});
-	//标签页
+	//标签页 
 	//$('#myTab li:eq(0) a').tab('show');
 	$(".nav_list>li").click(function(event) {
 		$(this).addClass('active').siblings('li').removeClass('active');
